0bf1777afadab98483ea734e6c1aee8b01935128
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-09-03 17:40:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,18 +58,12 @@ STICKER = [
 #### ❖ ──────⊱◈◈◈⊰────── ❖
 
 IMG = [
-"https://graph.org/file/eaa3a2602e43844a488a5.jpg",
-"https://graph.org/file/b129e98b6e5c4db81c15f.jpg",
-"https://graph.org/file/3ccb86d7d62e8ee0a2e8b.jpg",
-"https://graph.org/file/df11d8257613418142063.jpg",
-"https://graph.org/file/9e23720fedc47259b6195.jpg",
-"https://graph.org/file/826485f2d7db6f09db8ed.jpg",
-"https://graph.org/file/ff3ad786da825b5205691.jpg",
-"https://graph.org/file/52713c9fe9253ae668f13.jpg",
-"https://graph.org/file/8f8516c86677a8c91bfb1.jpg",
-"https://graph.org/file/6603c3740378d3f7187da.jpg",
-"https://graph.org/file/66cb6ec40eea5c4670118.jpg",
-"https://graph.org/file/2e3cf4327b169b981055e.jpg",
+"https://telegra.ph/file/451a8e1a363dc17c89556.jpg",
+"https://telegra.ph/file/2f9d2ab8e21b06a78f023.jpg",
+"https://telegra.ph/file/480cb450b3a545866ee82.jpg",
+"https://telegra.ph/file/bbda2b81f3a6786e22d2f.jpg",
+"https://telegra.ph/file/aab21db68216a76b2bfc2.jpg",
+    
 ]
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
